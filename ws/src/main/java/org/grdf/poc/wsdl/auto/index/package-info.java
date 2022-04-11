e1c42b1d3dc4abbb50dc32f959e9ef5257bd66b2
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2022-04-11 14:11:25 +0200

    Have removed generated files

--- ws/src/main/java/org/grdf/poc/wsdl/auto/index/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "com.grdf.poc.indexAutoReleve.V1.1.common", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.grdf.poc.wsdl.auto.index;
